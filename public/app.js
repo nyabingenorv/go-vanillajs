5e39ce8df5232caf77e0b1b567a8e7a07245ee15
Maximiliano Firtman <firtman@gmail.com>
2025-03-25 15:56:10 -0500

    client-side initial version

---
+++ public/app.js
@@ -0,0 +1,10 @@
+import { API } from "./services/API.js";
+
+window.app = {
+    search: (event) => {
+        event.preventDefault();
+        const q = document.querySelector("input[type=search]").value;
+        // TODO
+    },
+    api: API
+}
