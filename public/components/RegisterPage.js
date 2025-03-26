a83a59451951ed222680b5ffb96ae27a033bc36b
Maximiliano Firtman <firtman@gmail.com>
2025-03-26 14:57:11 -0500

    registration form

---
+++ public/components/RegisterPage.js
@@ -0,0 +1,10 @@
+export class RegisterPage extends HTMLElement {
+
+    connectedCallback() {
+        const template = document.getElementById("template-register");
+        const content = template.content.cloneNode(true);
+        this.appendChild(content);  
+    }
+}
+
+customElements.define("register-page", RegisterPage)
--- public/services/Routes.js
+++ public/services/Routes.js
@@ -1,6 +1,7 @@
 import { HomePage } from "../components/HomePage.js"
 import { MovieDetailsPage } from "../components/MovieDetailsPage.js"
 import { MoviesPage } from "../components/MoviesPage.js"
+import { RegisterPage } from "../components/RegisterPage.js"
 
 export const routes = [
     {
@@ -15,5 +16,9 @@ export const routes = [
     {
         path: "/movies",  // search results
         component: MoviesPage
+    },
+    {
+        path: "/account/register",
+        component: RegisterPage
     }
 ]
